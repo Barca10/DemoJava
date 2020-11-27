913d280332c75be6a0ce886741f64f686726a047
bittersweetmemories <nepal_arvind9@hotmail.com>
2020-11-27 14:16:25 -0600

    new commit

---
+++ src/main/java/com/example/Student/Model/Student.java
@@ -0,0 +1,16 @@
+package com.example.Student.Model;
+
+import com.example.Student.DBModel.StudentDTOModel;
+
+public class Student extends StudentDTOModel {
+	
+	public String FirstName;
+	public String LastName;
+	
+//	public Student() {
+//		super();
+//	}  
+	
+	
+	 	
+}
